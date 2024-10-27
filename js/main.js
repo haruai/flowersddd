961e59e7bbb70556f22892abe426f11ebde198b5
hawu <37008343+haruai@users.noreply.github.com>
2024-10-27 00:47:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('ILY SADIE').split('')
+    const titles = ('FOR SADIE').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
